74c43c284dc97a294654065efd05c60b563ee6f9
Tomasz Ziajko <tomasz.ziajko@gmail.com>
2021-09-07 17:43:47 +0200

    Cleanup test.

--- test/uds_test.cc
+++ test/uds_test.cc
@@ -4,15 +4,6 @@
 #include "Uds.h"
 
 TEST(HelloTest, BasicAssertions) {
-
-    auto wejscie = new std::istringstream("22\n0");
-
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
-    auto obiekt = new Uds::Service();
-    obiekt->getId();
-    *wejscie >> *obiekt;
-    EXPECT_EQ(obiekt->getId(),0x22);
+    auto WriteDataById = new Uds::WriteDataByIdentifier();
+    EXPECT_EQ(WriteDataById->getId(),0x22);
 }
